ab80150ac5704f7aeda5b5aa1051629437ebc006
codebykish <kishan_letters@yahoo.com>
2017-10-10 15:16:31 +0200

    Create class Events - Validations - Create releations users and events

---
+++ app/models/event.rb
@@ -0,0 +1,16 @@
+class Event < ApplicationRecord
+
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :capacity, presence: true
+  validates :includes_food, presence: true
+  validates :includes_drink, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates :active, presence: true
+
+end
