39f6160fcfb1b7ee42a867426e3585caf734293f
codebykish <kishan_letters@yahoo.com>
2017-10-13 15:01:50 +0200

    Trough Association: Registration Controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,17 @@
+class RegistrationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+      @bregistration = current_user.registrations.create(registration_params)
+
+      redirect_to @registration.event, notice: "Thank you for registering!"
+    end
+
+    private
+
+  def registration_params
+     params.require(:registration).permit(:active, :price, :guests_count, :event_id)
+   end
+
+
+end
